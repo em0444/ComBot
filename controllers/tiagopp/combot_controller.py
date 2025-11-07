ec15dd05caa73ac6efc1c6a1bf2b493bd96dc35e
Hannah Blyton <hannah.blyton@gmail.com>
2025-11-07 15:47:20 +0000

    control with arrow keys (so i can test localisation)

--- controllers/tiagopp/combot_controller.py
+++ controllers/tiagopp/combot_controller.py
@@ -1,17 +1,45 @@
 """tiagopp controller."""
 
-from controller import wb as c_webots_api #The wb package gives you all the C-like methods, but the controller package wraps most of them in nicer-to-use classes.
+from controller import wb as c_webots_api, \
+    Motor  # The wb package gives you all the C-like methods, but the controller package wraps most of them in nicer-to-use classes.
 from controllers.tiagopp.initialisation import initialise_motors
 from controllers.tiagopp.combot import Combot
 
 wb = c_webots_api.wb
 
 combot: Combot = Combot()
-combot.get_position()
+
+def check_keyboard():
+    max_speed = combot.getDevice("wheel_left_joint").getMaxVelocity()
+    key = wb.wb_keyboard_get_key()
+    speed_left, speed_right = 0, 0
+
+    if key == 315: #up
+        speed_left = max_speed
+        speed_right = max_speed
+    elif key == 317: #down
+        speed_left = -max_speed
+        speed_right = -max_speed
+    elif key == 316:  # right
+        speed_left = max_speed
+        speed_right = -max_speed
+    elif key == 314:  # left
+        speed_left = -max_speed
+        speed_right = max_speed
+
+    if key > 0:
+        combot.getDevice("wheel_left_joint").setPosition(float('inf'))
+        combot.getDevice("wheel_right_joint").setPosition(float('inf'))
+        combot.getDevice("wheel_left_joint").setVelocity(speed_left)
+        combot.getDevice("wheel_right_joint").setVelocity(speed_right)
+
+# combot.get_position()
 
 timestep = int(combot.getBasicTimeStep())
 
 initialise_motors()
+
+wb.wb_keyboard_enable(timestep)
 
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
@@ -24,6 +52,9 @@ while combot.step(timestep) != -1:
 
     # Enter here functions to send actuator commands, like:
     #  motor.setPosition(10.0)
+
+    check_keyboard()
+
     pass
 
 # Enter here exit cleanup code.
--- controllers/tiagopp/localisation.py
+++ controllers/tiagopp/localisation.py
@@ -1,10 +1,17 @@
 from typing import List
 
+from controller import PositionSensor, DistanceSensor
 from controllers.tiagopp.combot import Combot
 
 combot = Combot()
 
 def get_position() -> List[float]:
-    for device in combot.devices.values():
-        print(type(device))
+
+    position_sensors: List[PositionSensor] = [device for device in combot.devices.values() if isinstance(device, PositionSensor)]
+    distance_sensors: List[DistanceSensor] = [device for device in combot.devices.values() if isinstance(device, DistanceSensor)]
+    # lidar_sensors = List
+
+    for sensor in distance_sensors:
+        print(sensor.name)
+
     raise NotImplementedError()
