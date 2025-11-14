919a340682535bf4097f1a6617e0fa78c9baa853
Ella Macks <exm206@student.bham.ac.uk>
2025-11-14 12:44:39 +0000

    Adding function to allow manual fencing action usage

--- controllers/tiagopp/tiagopp.py
+++ controllers/tiagopp/tiagopp.py
@@ -3,15 +3,15 @@
 from controller import wb as c_webots_api, \
     Motor  # The wb package gives you all the C-like methods, but the controller package wraps most of them in nicer-to-use classes.
 from initialisation import initialise_motors
+from fencing_actions import lunge, parry_high, parry_low, en_garde, move_to_pose
 from combot import Combot
 
 wb = c_webots_api.wb
 
 combot: Combot = Combot()
 
-def check_keyboard():
+def check_keyboard(key):
     max_speed = combot.getDevice("wheel_left_joint").getMaxVelocity()
-    key = wb.wb_keyboard_get_key()
     speed_left, speed_right = 0, 0
 
     if key == 315: #up
@@ -25,22 +25,31 @@ def check_keyboard():
         speed_right = -max_speed
     elif key == 314:  # left
         speed_left = -max_speed
-        speed_right = max_speed
-
+        speed_right = max_speed     
     if key > 0:
         combot.getDevice("wheel_left_joint").setPosition(float('inf'))
         combot.getDevice("wheel_right_joint").setPosition(float('inf'))
         combot.getDevice("wheel_left_joint").setVelocity(speed_left)
         combot.getDevice("wheel_right_joint").setVelocity(speed_right)
+            
+def check_manual_fencing_action(key_code):
+
+    if key_code == 32:  # Spacebar (Lunge)
+        lunge()
+    elif key_code == 81:  # Q (Parry High)
+        parry_high() 
+    elif key_code == 90:  # Z (Parry Low)
+        parry_low()
+    elif key_code == 82:  # R (En Guard)
+        en_garde()
 
 # combot.get_position()
 
 timestep = int(combot.getBasicTimeStep())
 
-initialise_motors()
-
 wb.wb_keyboard_enable(timestep)
 
+initialise_motors()
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
 while combot.step(timestep) != -1:
@@ -52,8 +61,10 @@ while combot.step(timestep) != -1:
 
     # Enter here functions to send actuator commands, like:
     #  motor.setPosition(10.0)
+    key = wb.wb_keyboard_get_key()
 
-    check_keyboard()
+    check_keyboard(key)
+    check_manual_fencing_action(key)
 
     pass
 
