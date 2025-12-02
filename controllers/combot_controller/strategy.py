6d74f3454d94be150541bdad9ef934c3ef7f5348
grumpman <87781221+grumpman@users.noreply.github.com>
2025-12-02 13:35:54 +0000

    Added strategies up to 10 as well as more default strats

--- controllers/combot_controller/strategy.py
+++ controllers/combot_controller/strategy.py
@@ -1,12 +1,33 @@
-from fencing_actions import lunge, parry_high, parry_low, en_garde
+from fencing_actions import lunge, parry_high, parry_low, en_garde, move_backward, move_forward, move_stop
 import random
+from threading import Thread
 
 HITRANGE = 1.0
-ALLACTIONS =  [lunge, parry_high,parry_low,en_garde]
+ALLBODYACTIONS =  [lunge, parry_high,parry_low,en_garde]
 STRATEGY5ACTIONDELAY = 2000
 
 timeEllapsed = 0
 
+def mergeActions(action1,action2):
+    def executeBothActions():
+        first = Thread(target = action1)
+        second = Thread(target = action2)
+        first.start()
+        second.start()
+    return executeBothActions
+
+def mergeStrategies(strat1,strat2):
+    def newMergedStrategy(robot):
+        baseMove = strat1(robot)
+        bodyMove = strat2(robot)
+        if baseMove is None:
+            if bodyMove is None:
+                return None
+            return bodyMove
+        if bodyMove is None:
+            return baseMove
+        return mergeActions(baseMove,bodyMove)
+    return newMergedStrategy
 
 def distance(vec1,vec2):
     return ((vec1[0]-vec2[0])^2 + (vec1[1]-vec2[1])^2 + (vec1[2]-vec2[2])^2)^0.5
@@ -43,21 +64,21 @@ def strategy4(robot):
     return None
 
 def templateStrategyCycle(actionList,timeInterval):
-    def newStrategy(robot):
-        if not robot.changing_body_state and robot.timeCounter<=robot.getTime():
+    def newCycleStrategy(robot):
+        if robot.timeCounter<=robot.getTime():
             robot.timeCounter = robot.timeCounter + timeInterval
             robot.cycleIndex = robot.cycleIndex + 1
             if robot.cycleIndex >= len(actionList):
                 robot.cycleIndex = 0
             return actionList[robot.cycleIndex]
         return None
-    return newStrategy
+    return newCycleStrategy
 
 def templateStrategyRandom(actionList,weights,timeInterval):
     if len(actionList)!=len(weights):
         return strategy0
     def newStrategy(robot):
-        if not robot.changing_body_state and robot.timeCounter<=robot.getTime():
+        if robot.timeCounter<=robot.getTime():
             robot.timeCounter = robot.timeCounter + timeInterval
             weightIndex = random.randint(1,sum(weights))
             for i in range(0,len(actionList)):
@@ -68,8 +89,25 @@ def templateStrategyRandom(actionList,weights,timeInterval):
         return None
     return newStrategy
 
-strategy5 = templateStrategyRandom(ALLACTIONS,[1,1,1,1],2.0)
+strategy5 = templateStrategyRandom(ALLBODYACTIONS,[1,1,1,1],2.0)
 
-strategy6 = templateStrategyRandom(ALLACTIONS,[1,1,1,1],1.0)
+strategy6 = templateStrategyRandom(ALLBODYACTIONS,[1,1,1,1],1.0)
 
 strategy7 = templateStrategyCycle([parry_high,parry_low],1.5)
+
+def moveForwardStrategy(robot):
+    if not robot.changing_base_state and robot.base_state != "FORWARD":
+        return move_forward
+    return None
+
+def moveBackwardStrategy(robot):
+    if not robot.changing_base_state and robot.base_state != "BACKWARD":
+        return move_backward
+    return None
+
+strategy8 = mergeStrategies(strategy1,moveForwardStrategy)
+
+strategy9 = mergeStrategies(strategy2,moveForwardStrategy)
+
+strategy10 = templateStrategyCycle([mergeActions(move_forward,lunge),move_stop,mergeActions(move_backward,en_garde),move_stop],1.5)
+
