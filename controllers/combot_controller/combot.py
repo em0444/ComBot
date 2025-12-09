80b0c4b0661f061d45e9eb4b2f2b04b50ef96486
grumpman <87781221+grumpman@users.noreply.github.com>
2025-12-09 01:31:35 +0000

    Added functions for neural network

--- controllers/combot_controller/combot.py
+++ controllers/combot_controller/combot.py
@@ -12,6 +12,13 @@ class Combot(Supervisor):
     base_state = "STILL"
     timeCounter = 0.0
     cycleIndex = -1
+    previousPosition = -2.0
+    previouspreviousPosition = -2.0
+    previousVelocity = 0.0
+    enemyPosition = 2.0
+    previousEnemyPosition = 2.0
+    previouspreviousEnemyPosition = 2.0
+    previousEnemyVelocity = 0.0
 
     def __new__(cls, *args, **kwargs):
         if cls._instance is None:
@@ -33,6 +40,9 @@ class Combot(Supervisor):
         if self.localisation is None:
             from localisation import Localisation
             self.localisation = Localisation(combot_obj=self)
+        self.previouspreviousPosition = self.previousPosition
+        self.previousPosition = self.position
+        self.previousVelocity = self.previousPosition-self.previouspreviousPosition
         self.position = self.localisation.update_internal_position_model()
 
     def get_position(self) -> Position:
@@ -40,6 +50,19 @@ class Combot(Supervisor):
             from localisation import Localisation
             self.localisation = Localisation(combot_obj=self)
         return self.position
+    
+    def get_x_velocity(self):
+        return (self.position.x-self.previousPosition.x)
+    
+    def get_z_velocity(self):
+        return (self.position.x-self.previousPosition.x)
+    
+    def get_angular_velocity(self):
+        return (self.position.heading_in_radians-self.previousPosition.heading_in_radians)
+    
+    def get_x_acceleration(self):
+        return (self.get_x_velocity-self.previousVelocity)
+    
 
     def move_to_position(self, position: Position, counter: int) -> bool:
         """
@@ -67,9 +90,17 @@ class Combot(Supervisor):
         if self.localisation is None:
             from localisation import Localisation
             self.localisation = Localisation(combot_obj=self)
-        self.position = self.localisation.get_enemy_position()
-
-        raise NotImplementedError()
+        self.previouspreviousEnemyPosition = self.previousEnemyPosition
+        self.previousEnemyPosition = self.enemyPosition
+        self.previousEnemyVelocity = self.previousEnemyPosition-self.previouspreviousEnemyPosition
+        self.enemyPosition = self.localisation.get_enemy_position()
+        return self.enemyPosition
+    
+    def get_enemy_x_velocity(self):
+        return self.enemyPosition-self.previousEnemyPosition
+    
+    def get_enemy_x_acceleration(self):
+        return self.get_enemy_x_velocity()-self.previousEnemyVelocity
     
     def get_enemy_arm_position(self):
         raise NotImplementedError()
